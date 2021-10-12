4c6ffc971af993beceff2d0ff11626e0fd545236
andochiwa <a1066079469@gmail.com>
2021-10-12 16:12:31 +0900

    [fix] router backend uri

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,7 @@ router.beforeEach((to, from, next) => {
     next()
   } else {
     http({
-      url: http.adornUrl('/sys/menu/nav'),
+      url: http.adornUrl('/admin/sys/menu/nav'),
       method: 'get',
       params: http.adornParams()
     }).then(({data}) => {
