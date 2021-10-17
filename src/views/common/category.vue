aad53166c146c6e59610f0891836c02190ee3601
andochiwa <a1066079469@gmail.com>
2021-10-18 03:07:16 +0900

    [feat] created common category tree

---
+++ src/views/common/category.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    node-key="id"
+    ref="menuTree"
+  >
+  </el-tree>
+</template>
+
+<script>
+import category from "@/api/product/category";
+
+export default {
+  data() {
+    return {
+      expendedKey: [],
+      menus: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  created() {
+    this.getTreeData()
+  },
+  methods: {
+    async getTreeData() {
+      const {data} = await category.getTreeData();
+      this.menus = data.data.category;
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
