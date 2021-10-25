e0030a894685b4d25b12b4c18b77815c8f284005
andochiwa <a1066079469@gmail.com>
2021-10-25 23:05:43 +0900

    [fix] hidden routing

--- src/router/index.js
+++ src/router/index.js
@@ -4,49 +4,75 @@
  * 建议:
  * 1. 代码中路由统一使用name属性跳转(不使用path属性)
  */
-import Vue from 'vue'
-import Router from 'vue-router'
-import http from '@/utils/httpRequest'
-import { isURL } from '@/utils/validate'
-import { clearLoginInfo } from '@/utils'
+import Vue from "vue";
+import Router from "vue-router";
+import http from "@/utils/httpRequest";
+import {isURL} from "@/utils/validate";
+import {clearLoginInfo} from "@/utils";
 
-Vue.use(Router)
+Vue.use(Router);
 
 // 开发环境不使用懒加载, 因为懒加载页面太多的话会造成webpack热更新太慢, 所以只有生产环境使用懒加载
 const _import = require('./import-' + process.env.NODE_ENV)
 
 // 全局路由(无需嵌套上左右整体布局)
 const globalRoutes = [
-  { path: '/404', component: _import('common/404'), name: '404', meta: { title: '404未找到' } },
-  { path: '/login', component: _import('common/login'), name: 'login', meta: { title: '登录' } }
+  {path: '/404', component: _import('common/404'), name: '404', meta: {title: '404未找到'}},
+  {path: '/login', component: _import('common/login'), name: 'login', meta: {title: '登录'}}
 ]
 
 // 主入口路由(需嵌套上左右整体布局)
 const mainRoutes = {
-  path: '/',
-  component: _import('main'),
-  name: 'main',
-  redirect: { name: 'home' },
-  meta: { title: '主入口整体布局' },
+  path: "/",
+  component: _import("main"),
+  name: "main",
+  redirect: { name: "home" },
+  meta: { title: "主入口整体布局" },
   children: [
     // 通过meta对象设置路由展示方式
     // 1. isTab: 是否通过tab展示内容, true: 是, false: 否
     // 2. iframeUrl: 是否通过iframe嵌套展示内容, '以http[s]://开头': 是, '': 否
     // 提示: 如需要通过iframe嵌套展示内容, 但不通过tab打开, 请自行创建组件使用iframe处理!
-    { path: '/home', component: _import('common/home'), name: 'home', meta: { title: '首页' } },
-    { path: '/theme', component: _import('common/theme'), name: 'theme', meta: { title: '主题' } },
-    { path: '/demo-echarts', component: _import('demo/echarts'), name: 'demo-echarts', meta: { title: 'demo-echarts', isTab: true } },
-    { path: '/demo-ueditor', component: _import('demo/ueditor'), name: 'demo-ueditor', meta: { title: 'demo-ueditor', isTab: true } }
-  ],
-  beforeEnter (to, from, next) {
-    let token = Vue.cookie.get('token')
-    if (!token || !/\S/.test(token)) {
-      clearLoginInfo()
-      next({ name: 'login' })
+    {
+      path: "/home",
+      component: _import("common/home"),
+      name: "home",
+      meta: { title: "首页" }
+    },
+    {
+      path: "/theme",
+      component: _import("common/theme"),
+      name: "theme",
+      meta: { title: "主题" }
+    },
+    {
+      path: "/demo-echarts",
+      component: _import("demo/echarts"),
+      name: "demo-echarts",
+      meta: { title: "demo-echarts", isTab: true }
+    },
+    {
+      path: "/demo-ueditor",
+      component: _import("demo/ueditor"),
+      name: "demo-ueditor",
+      meta: { title: "demo-ueditor", isTab: true }
+    },
+    {
+      path: "/product-attrupdate",
+      component: _import("modules/product/attrupdate"),
+      name: "attr-update",
+      meta: { title: "规格维护", isTab: true }
     }
-    next()
+  ],
+  beforeEnter(to, from, next) {
+    let token = Vue.cookie.get("token");
+    if (!token || !/\S/.test(token)) {
+      clearLoginInfo();
+      next({ name: "login" });
+    }
+    next();
   }
-}
+};
 
 const router = new Router({
   mode: 'hash',
